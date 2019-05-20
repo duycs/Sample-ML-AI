e7d424e64ef6c454cbb864c23f60c83e8aa84fab
duycs <duycs@outlook.com>
2019-05-20 20:37:53 +0700

    update handle logistic_tf.py

--- vietai-assignment1/logistic_tf.py
+++ vietai-assignment1/logistic_tf.py
@@ -37,25 +37,28 @@ if __name__ == "__main__":
     test_x = add_one(test_x)
    
     # [TODO 1.11] Create TF placeholders to feed train_x and train_y when training
-    x = None 
-    y = None 
+    x = tf.placeholder(dtype=tf.float64)
+    y = tf.placeholder(dtype=tf.float64)
+
 
     # [TODO 1.12] Create weights (W) using TF variables
     w_shape = (train_x.shape[1],1)
-    w = None 
+    w = tf.Variable(np.random.normal(0, np.sqrt(2./np.sum(w_shape)), w_shape))
+
 
     # [TODO 1.13] Create a feed-forward operator
-    pred = None 
+    pred = tf.sigmoid(tf.matmul(x, w))
 
     # [TODO 1.14] Write the cost function
-    cost = None 
+    cost = tf.reduce_mean(-(y * tf.log(pred) + (1 - y) * tf.log(1 - pred)))
 
     # Define hyper-parameters and train-related parameters
     num_epoch = 1000
     learning_rate = 0.01    
 
     # [TODO 1.15] Create an SGD optimizer
-    optimizer = None 
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
 
     # Some meta parameters
     epochs_to_draw = 100
@@ -71,7 +74,8 @@ if __name__ == "__main__":
 
         for e in range(num_epoch):
             # [TODO 1.16] Compute loss and update weights here
-            loss = 0
+            #loss = 0
+            _, loss = sess.run([optimizer, cost], feed_dict={x: train_x, y: train_y})
             # Update weights...
 
             all_loss.append(loss)
